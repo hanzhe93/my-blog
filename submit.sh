171d51742dfcb043d44db59677eb868b9270d707
hanzhe93 <hanzhe93@gmail.com>
2025-04-16 15:50:06 +0200

    change comments in submit.sh

--- submit.sh
+++ submit.sh
@@ -5,7 +5,7 @@ git commit -m "${1:-Update content}"
 
 # try first git push
 if ! git push origin main; then
-  echo "Initial push failed. Attempting to rebase with 'origin/dev'..."
+  echo "Initial push failed. Attempting to rebase with 'origin/main'..."
   git pull --rebase origin main
 
   # retry git push
