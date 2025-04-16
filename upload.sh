d8162965b3ab5e3245a15ed3ba8f8a02fa90701d
hanzhe93 <hanzhe93@gmail.com>
2025-04-16 15:32:42 +0200

    test upload.sh

--- upload.sh
+++ upload.sh
@@ -6,7 +6,7 @@ git commit -m "${1:-Update content}"
 # 尝试第一次 push
 if ! git push origin main; then
   echo "Initial push failed. Attempting to rebase with 'origin/dev'..."
-  git pull --rebase origin dev
+  git pull --rebase origin main
 
   # 再次尝试 push
   git push origin main
